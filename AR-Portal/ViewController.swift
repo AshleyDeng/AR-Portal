4d129142204621cac6a69aa7d6b4ac9549a1c9b9
Dongcheng Deng <dongcheng.d@gmail.com>
2018-04-20 14:21:48 -0600

    first commit

--- AR-Portal/ViewController.swift
+++ AR-Portal/ViewController.swift
@@ -7,19 +7,86 @@
 //
 
 import UIKit
+import ARKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, ARSCNViewDelegate {
 
+  @IBOutlet weak var sceneView: ARSCNView!
+  @IBOutlet weak var planeDetected: UILabel!
+  
+  let configuration = ARWorldTrackingConfiguration()
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    configuration.planeDetection = .horizontal
+    
+    sceneView.delegate = self
+    sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+    sceneView.session.run(configuration)
+    
+    let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+    sceneView.addGestureRecognizer(tapGestureRecognizer)
+  }
+  
+  @objc func handleTap(sender: UITapGestureRecognizer) {
+    guard let sceneView = sender.view as? ARSCNView else {return}
+    let touchLocation = sender.location(in: sceneView)
+    
+    let hitTestResult = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
+    if !hitTestResult.isEmpty {
+      addPortal(hitTestResult: hitTestResult.first!)
+    }
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  func addPortal(hitTestResult: ARHitTestResult) {
+    let portalScene = SCNScene(named: "Portal.scnassets/Portal.scn")
+    let portalNode = portalScene?.rootNode.childNode(withName: "Portal", recursively: false)
+    
+    let transform = hitTestResult.worldTransform
+    let planeXposition = transform.columns.3.x
+    let planeYposition = transform.columns.3.y
+    let planeZposition = transform.columns.3.z
+    
+    portalNode?.position = SCNVector3(planeXposition, planeYposition, planeZposition)
+    sceneView.scene.rootNode.addChildNode(portalNode!)
+    addPlane(nodeName: "roof", portalNode: portalNode!, imageName: "top")
+    addPlane(nodeName: "floor", portalNode: portalNode!, imageName: "bottom")
+    addWalls(nodeName: "backWall", portalNode: portalNode!, imageName: "back")
+    addWalls(nodeName: "sideWallA", portalNode: portalNode!, imageName: "sideA")
+    addWalls(nodeName: "sideWallB", portalNode: portalNode!, imageName: "sideB")
+    addWalls(nodeName: "sideDoorA", portalNode: portalNode!, imageName: "sideDoorA")
+    addWalls(nodeName: "sideDoorB", portalNode: portalNode!, imageName: "sideDoorB")
   }
-
+  
+  func addWalls(nodeName: String, portalNode: SCNNode, imageName: String) {
+    let child = portalNode.childNode(withName: nodeName, recursively: true)
+    child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
+    child?.renderingOrder = 200
+    
+    if let mask = child?.childNode(withName: "mask", recursively: false) {
+      mask.geometry?.firstMaterial?.transparency = 0.000001
+    }
+    
+  }
+  
+  func addPlane(nodeName: String, portalNode: SCNNode, imageName: String) {
+    let child = portalNode.childNode(withName: nodeName, recursively: true)
+    child?.geometry?.firstMaterial?.diffuse.contents = UIImage(named: "Portal.scnassets/\(imageName).png")
+    child?.renderingOrder = 200
+  }
+  
+  func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+    guard anchor is ARPlaneAnchor else { return }
+    
+    DispatchQueue.main.async {
+      self.planeDetected.isHidden = false
+    }
+    
+    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+      self.planeDetected.isHidden = true
+    }
+  }
 
 }
 
